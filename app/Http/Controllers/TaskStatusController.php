38f90d5a664495ba1a76f1eb11673df22305fbb4
funkai13 <evinssonmedrano@gmail.com>
2024-03-24 17:54:20 -0600

    TaskStatus Controller Update

--- app/Http/Controllers/TaskStatusController.php
+++ app/Http/Controllers/TaskStatusController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\TaskStatus;
 use App\Http\Requests\StoreTaskStatusRequest;
 use App\Http\Requests\UpdateTaskStatusRequest;
-
+use App\Http\Responses\ApiResponse;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 class TaskStatusController extends Controller
 {
     /**
@@ -13,7 +16,13 @@ class TaskStatusController extends Controller
      */
     public function index()
     {
-        //
+        try {
+            $taskstatus = TaskStatus::all();
+            return ApiResponse::success('Taskstatus list',200,$taskstatus);
+
+        }catch (Exception $e) {
+            return  ApiResponse::error('Error getting taskstatus list',500); 
+        }
     }
 
     /**
@@ -27,9 +36,22 @@ class TaskStatusController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreTaskStatusRequest $request)
+    public function store(Request $request)
     {
-        //
+        try {
+            $request->validate([
+                'name', 
+                'description', 
+                'created_by', 
+                'active',
+                
+            ]);
+            $taskstatus = TaskStatus::create($request->all());
+           return ApiResponse::success('task status Create',201,$taskstatus);
+
+        } catch (ValidationException $e) {
+            return ApiResponse:: error('validation error',422,);
+        }
     }
 
     /**
@@ -37,7 +59,7 @@ class TaskStatusController extends Controller
      */
     public function show(TaskStatus $taskStatus)
     {
-        //
+        return $taskStatus;
     }
 
     /**
@@ -51,16 +73,34 @@ class TaskStatusController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateTaskStatusRequest $request, TaskStatus $taskStatus)
+    public function update(Request $request,  $id)
     {
-        //
+        try {
+            $taskstatus = TaskStatus::findOrFail($id);
+            $request->validate([
+                'name', 
+                'description', 
+                'created_by', 
+                'active',
+                
+            ]);
+            $taskstatus ->update($request->all());
+        } catch (ValidationException $e) {
+             return ApiResponse:: error('validation error',422,);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(TaskStatus $taskStatus)
+    public function destroy( $id)
     {
-        //
+        try {
+            $taskstatus = TaskStatus::findOrFail($id);
+            $taskstatus->delete();
+            return ApiResponse::success('Deleted TaskStatus');
+        } catch (Exception $e) {
+            return ApiResponse::error('TaskStatus not found',404);
+        }
     }
 }
