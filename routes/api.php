6a62f8f7ee6656fb7f2b7152475a38e6152658ce
funkai13 <evinssonmedrano@gmail.com>
2024-03-25 18:03:42 -0600

    Add register route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+// Auth Controller
+use App\Http\Controllers\AuthController;
+
 use App\Http\Controllers\CommentTaskController;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\TaskController;
@@ -14,6 +17,9 @@ Route::apiResource('task_statuses',TaskStatusController::class);
 Route::apiResource('comment_tasks',CommentTaskController::class);
 
 Route::get('task_statuses/{status}/task',[TaskStatusController::class,'taskByStatus']);
+
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register', [AuthController::class, 'register']);
