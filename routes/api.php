957006a63500c6c4815c3f3d64294418e8407cac
funkai13 <evinssonmedrano@gmail.com>
2024-03-25 18:14:12 -0600

    Add login route

--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,4 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 });
 
 Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
