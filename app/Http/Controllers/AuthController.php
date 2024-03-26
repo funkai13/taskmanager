11dab466bf63da16f928bafe5945a7d617f19334
funkai13 <evinssonmedrano@gmail.com>
2024-03-25 18:14:05 -0600

    Login logic added

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,20 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     //
-    public function register(Request $request) {
-        $validator = Validator::make($request -> all(), [
+    public function register(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8'
         ]);
 
-        if ($validator -> fails()) {
-            return response() -> json($validator -> errors());
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
         }
 
         $data = $validator->getData();
@@ -25,12 +27,43 @@ class AuthController extends Controller
 
         $user = User::create($userData);
 
-        $token = $user -> createToken('auth_token') -> plainTextToken;
+        $token = $user->createToken('auth_token')->plainTextToken;
 
-        return response() -> json([
+        return response()->json([
             'data' => $user,
             'access_token' => $token,
             'token_type' => 'Bearer'
+        ]);
+    }
+
+    public function login(Request $request): \Illuminate\Foundation\Application|\Illuminate\Http\Response|\Illuminate\Http\JsonResponse|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        $data = $validator->getData();
+        $user = User::where('name', $data['name'])->first();
+
+        if (!$user || !Hash::check($data['password'], $user->password)) {
+            return response([
+                'error' => true,
+                'message' => 'Wrong credentials'
+            ]);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'data' => $user,
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'role' => $user->role
         ]);
     }
 }
