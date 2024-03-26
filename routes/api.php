8692443b043e257bb4dc445d191ee12f349ceb5e
funkai13 <evinssonmedrano@gmail.com>
2024-03-26 10:42:06 -0600

    Add register route to role admin

--- routes/api.php
+++ routes/api.php
@@ -7,11 +7,8 @@ use App\Http\Controllers\CommentTaskController;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TaskStatusController;
-use App\Http\Middleware\CheckRole;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('task_statuses/{status}/task',[TaskStatusController::class,'taskByStatus']);
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
@@ -29,7 +26,9 @@ Route::group(['middleware' => ['auth:sanctum']], function(){
     Route::apiResource('tasks',TaskController::class);
     Route::apiResource('task_statuses',TaskStatusController::class);
     Route::apiResource('comment_tasks',CommentTaskController::class);
+    Route::get('task_statuses/{status}/task', [TaskStatusController::class,'taskByStatus']);
 
     // Admin Routes
     Route::apiResource('employees',EmployeeController::class)->middleware('role:admin');
+    Route::post('auth/register', [AuthController::class, 'register'])->middleware('role:admin');
 });
